94b11df8d6eb8864def8d45e93c25f5abedcd46c
liuyu <liuy@ggxx.net>
2021-08-24 17:42:21 +0800

    feature: operators.go 1

--- operators.go
+++ operators.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// https://www.runoob.com/go/go-operators.html
 func main() {
 	var a int = 21
 	var b int = 10
@@ -22,6 +23,110 @@ func main() {
 	a = 21 // 为了方便测试，a 这里重新赋值为 21
 	a--
 	fmt.Printf("第七行 - a 的值为 %d\n", a)
+
+	logicOperator()
+	bitOperator()
+	giveOperator()
+	otherOperator()
 }
 
-//func logic
+func logicOperator() {
+	var a bool = true
+	var b bool = false
+	if a && b {
+		fmt.Printf("第一行 - 条件为 true\n")
+	}
+	if a || b {
+		fmt.Printf("第二行 - 条件为 true\n")
+	}
+	/* 修改 a 和 b 的值 */
+	a = false
+	b = true
+	if a && b {
+		fmt.Printf("第三行 - 条件为 true\n")
+	} else {
+		fmt.Printf("第三行 - 条件为 false\n")
+	}
+	if !(a && b) {
+		fmt.Printf("第四行 - 条件为 true\n")
+	}
+}
+
+func bitOperator() {
+
+	var a uint = 60 /* 60 = 0011 1100 */
+	var b uint = 13 /* 13 = 0000 1101 */
+	var c uint = 0
+
+	c = a & b /* 12 = 0000 1100 */
+	fmt.Printf("第一行 - c 的值为 %d\n", c)
+
+	c = a | b /* 61 = 0011 1101 */
+	fmt.Printf("第二行 - c 的值为 %d\n", c)
+
+	c = a ^ b /* 49 = 0011 0001 */
+	fmt.Printf("第三行 - c 的值为 %d\n", c)
+
+	c = a << 2 /* 240 = 1111 0000 */
+	fmt.Printf("第四行 - c 的值为 %d\n", c)
+
+	c = a >> 2 /* 15 = 0000 1111 */
+	fmt.Printf("第五行 - c 的值为 %d\n", c)
+}
+
+func giveOperator() {
+	var a int = 21
+	var c int
+
+	c = a
+	fmt.Printf("第 1 行 - =  运算符实例，c 值为 = %d\n", c)
+
+	c += a
+	fmt.Printf("第 2 行 - += 运算符实例，c 值为 = %d\n", c)
+
+	c -= a
+	fmt.Printf("第 3 行 - -= 运算符实例，c 值为 = %d\n", c)
+
+	c *= a
+	fmt.Printf("第 4 行 - *= 运算符实例，c 值为 = %d\n", c)
+
+	c /= a
+	fmt.Printf("第 5 行 - /= 运算符实例，c 值为 = %d\n", c)
+
+	c = 200
+
+	c <<= 2
+	fmt.Printf("第 6行  - <<= 运算符实例，c 值为 = %d\n", c)
+
+	c >>= 2
+	fmt.Printf("第 7 行 - >>= 运算符实例，c 值为 = %d\n", c)
+
+	c &= 2
+	fmt.Printf("第 8 行 - &= 运算符实例，c 值为 = %d\n", c)
+
+	c ^= 2
+	fmt.Printf("第 9 行 - ^= 运算符实例，c 值为 = %d\n", c)
+
+	c |= 2
+	fmt.Printf("第 10 行 - |= 运算符实例，c 值为 = %d\n", c)
+}
+
+func otherOperator() {
+	var a int = 4
+	var b int32
+	var c float32
+	var ptr *int
+
+	/* 运算符实例 */
+	fmt.Printf("第 1 行 - a 变量类型为 = %T\n", a)
+	fmt.Printf("第 2 行 - b 变量类型为 = %T\n", b)
+	fmt.Printf("第 3 行 - c 变量类型为 = %T\n", c)
+
+	/*  & 和 * 运算符实例 */
+	ptr = &a /* 'ptr' 包含了 'a' 变量的地址 */
+	fmt.Printf("a 的值为  %d\n", a)
+	fmt.Printf("*ptr 为 %d\n", *ptr)
+	fmt.Printf("ptr 为 %d\n", ptr)
+	fmt.Printf("&ptr 为 %d\n", &ptr)
+	fmt.Printf("&a 为 %d\n", &a)
+}
